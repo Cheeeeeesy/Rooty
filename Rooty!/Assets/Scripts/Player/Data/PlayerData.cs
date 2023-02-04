efaa515e2ef3fe1965d7af6df5c4e8daf2dbca76
Cheeeeeesy <54483248+Cheeeeeesy@users.noreply.github.com>
2023-02-04 10:05:11 +0100

    Startup

---
+++ Rooty!/Assets/Scripts/Player/Data/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newPlayerData", menuName = "Data/Player Data/Base Data")]
+public class PlayerData : ScriptableObject
+{
+    [Header("Move State")]
+    [SerializeField] private float moveSpeed;
+
+    public float MoveSpeed => moveSpeed;
+
+    [Header("Jump State")]
+    [SerializeField] private float jumpVelocity;
+
+    public float JumpVelocity => jumpVelocity;
+
+    [Header("In Air State")]
+    [SerializeField] private float inAirAcceleration;
+
+    public float InAirAcceleration => inAirAcceleration;
+
+    [Header("Check Variables")]
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private Vector2 groundCheckBox;
+
+    public LayerMask WhatIsGround => whatIsGround;
+    public Vector2 GroundCheckBox => groundCheckBox;
+}
